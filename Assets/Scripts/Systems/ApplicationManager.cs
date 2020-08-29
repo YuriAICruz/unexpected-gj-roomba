e8a6f864ccc5ee29f9f980a3f310a738561420da
Yuri <yuriaicruz@gmail.com>
2020-08-29 13:27:54 -0300

    apll manager

---
+++ Assets/Scripts/Systems/ApplicationManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Roomba.Systems
+{
+    public class ApplicationManager
+    {
+        public void OpenMenu()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void StartGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
