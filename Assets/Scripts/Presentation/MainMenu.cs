909d155efd5eddd03d6262017acf7baf9964205d
Matheus <matheus.mello@vetorzero.com.br>
2020-08-29 17:22:01 -0300

    start main menu

---
+++ Assets/Scripts/Presentation/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Roomba.Systems;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+namespace Roomba.Presentation
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [Inject] private ApplicationManager _manager;
+        [Inject] private InputCollector _input;
+        
+        public Button startGame, options;
+
+        private void Start()
+        {
+            startGame.onClick.AddListener(_manager.StartGame);
+            options.onClick.AddListener(_manager.OpenMenu);
+        }
+    }
+}
